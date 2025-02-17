6dc3d76e48d25c058b0a5ae2d30b747daea09b49
MatarCross <103392825+MatarCross@users.noreply.github.com>
2025-02-17 17:30:24 -0400

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,8 @@
+def invertir_numero():
+    numero = input("Ingrese un número entero: ")
+
+    numero_invertido = numero[::-1]
+    
+    print(f"El número invertido es: {numero_invertido}")
+
+invertir_numero()
